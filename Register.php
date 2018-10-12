8ac40d83793cefc236169744a7191871863b2498
linux490 <linux490@linux490-VirtualBox>
2018-10-11 21:53:04 -0400

    First Commit

---
+++ Register.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<script>
+
+function HandleRegistrationResponse(response)
+{
+	var text = JSON.parse(response);
+    //gotopage(text);
+}
+
+function SendRegistrationRequest()
+{
+	var newEmail = document.getElementById("newEmail").value;
+    var newUsername = document.getElementById('newUsername').value;
+    var newPassword = document.getElementById('newPassword').value;
+    var registrationRequest = new XMLHttpRequest();
+	registrationRequest.open("POST","login.php",true);
+	registrationRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	registrationRequest.onreadystatechange= function ()
+	{
+		
+		if ((this.readyState == 4)&&(this.status == 200))
+		{
+            HandleRegistrationResponse(this.responseText);
+		}
+        //document.write(newPassword);
+	}
+	registrationRequest.send("type=register&email="+newEmail+"&uname="+newUsername+"&pword="+newPassword);
+}
+function gotopage(canthey){
+    if(canthey == "1"){
+       document.location.href = "Homepage.php";
+    }
+    else{
+       //document.getElementById("textResponse").innerHTML = "response: Wrong password<p>";
+       }
+}
+    </script>
+<head>
+	<title>Login V3</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/league.png');">
+			<div class="wrap-login100">
+				<!--<form class="login100-form validate-form">-->
+
+
+					<span class="login100-form-title p-b-34 p-t-27">
+						Registration Form
+					</span>
+                    <div class="wrap-input100 validate-input">
+						<input class="input100" id="newEmail" type="text" name="email" placeholder="Email">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" id="newUsername" type="text" name="username" placeholder="Username">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" id="newPassword" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" id="passwordconfirm" type="password" name="passwordcofirm" placeholder="Confirm Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" onclick="SendRegistrationRequest()">
+							Register
+						</button>
+					</div>
+
+					<div class="text-center p-t-90">
+						<a class="txt1" href="index.html">
+							ALready have an account? Login.
+						</a>
+					</div>
+				<!--</form>-->
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,44 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+if (!isset($_POST))
+{
+	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
+	echo json_encode($msg);
+	exit(0);
+}
+$request = $_POST;
+$response = "unsupported request type, politely FUCK OFF";
+//echo $request["type"];
+switch ($request["type"])
+{
+	case "login":
+        $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+        $request2 = array();
+        $request2['type'] = "login";
+        $request2['username'] = $_POST['uname']; //$argv[1];
+        $request2['password'] = $_POST['pword'];//$argv[2];
+        $request2['message'] = "HI";
+        $response = $client->send_request($request2);
+        //$response = $client->publish($request);
+
+        //echo "client received response: ".PHP_EOL;
+        //print_r($response);
+        //echo "\n\n";*/
+            //$response = "login, yeah we can do that";
+	   break;
+    case "register":
+        $registerClient = new rabbitMQClient("testRabbitMQ.ini","testServer");
+        $registerRequest = array();
+        $registerRequest['type'] = "register";
+        $registerRequest['email'] = $_POST['email'];
+        $registerRequest['username'] = $_POST['uname']; //$argv[1];
+        $registerRequest['password'] = $_POST['pword'];//$argv[2];
+        $registerRequest['message'] = "HIIIII";
+        $response = $registerClient->send_request($registerRequest);
+    break;
+}
+echo json_encode($response);
+exit(0);
+?>
