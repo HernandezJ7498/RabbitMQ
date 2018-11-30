f6d92082349c078eecfafb0c9e76c0e668627578
hibby50 <hibby50@gmail.com>
2018-11-30 11:33:39 -0500

    Actually fix bugs and add stable flag for package builds

--- package maker/package.py
+++ package maker/package.py
@@ -10,10 +10,6 @@ Config.read("package.ini")
 Config.sections()
 ['frontEndPackage', 'backEndPackage', 'DMZPackage']
 
-cnx = mysql.connector.connect(user='test', password='4321Password.',
-                              host='192.168.0.11',
-                              database='deployment')
-
 def ConfigSectionMap(section):
     dict1 = {}
     options = Config.options(section)
@@ -27,38 +23,104 @@ def ConfigSectionMap(section):
             dict1[option] = None
     return dict1
 
-print("You can make a package for:")
+def install():
+    installIP = input("Enter the IP of the machine to install to: ")
+    selection = input("\n Enter a package name to install: ")
 
-for sections in Config.sections():
-    print(sections)
+    latestRollback = input ("Enter 1 to install latest package enter 2 to rollback: ")
+    if latestRollback == "1":
 
-selection = input("\n Enter a package name to build: ")
-directory = input("Enter your working directory e.g /home/directory/: \n")
+        mycursor2 = cnx.cursor()
 
+        mycursor2.execute("SELECT version FROM versions WHERE type='" + selection + "'")
 
-mycursor = cnx.cursor()
+        latest = mycursor2.fetchall()
 
-mycursor.execute("SELECT version FROM versions WHERE type='" + selection + "'")
+        versions = []
+        for version in latest:
+            versions.append(version[0])
+            print(versions)
 
-latest = mycursor.fetchall()
+        latestVersion = max(versions)
 
-versions=[]
-for version in latest:
-    versions.append(version[0])
-    print(versions)
+        packageName = selection + str(latestVersion) + ".tar.gz"
 
-prevVersion = max(versions)
+        os.system("scp " + "linux490@" + deploymentIP + ":/packages/" + selection + str(latestVersion) + ".tar.gz linux490@" + installIP + ":/packages")
+        os.system("ssh linux490@" + installIP + " 'tar xf /packages/" + packageName + " -C /packages/unzipped/'")
+        os.system("ssh linux490@" + installIP + " cp -a /packages/unzipped/" + selection.lower() + "/. /var/www/html")
 
-newVersion = prevVersion + 1
+    if latestRollback == "2":
+        mycursor = cnx.cursor()
 
-sql = "INSERT INTO versions (filename, type, version) VALUES (%s, %s, %s)"
-val = (selection + str(newVersion) + ".tar.gz", selection, newVersion)
-mycursor.execute(sql, val)
-cnx.commit()
+        mycursor.execute("SELECT version FROM versions WHERE type='" + selection + "' and stable=1")
+        latest = mycursor.fetchall()
 
-tar = tarfile.open(selection + str(newVersion) + ".tar.gz", "w:gz")
-for files in ConfigSectionMap(selection):
-    tar.add(files)
-tar.close()
+        versions = []
+        for version in latest:
+            versions.append(version[0])
+            print(versions)
 
-os.system("scp " + selection + str(newVersion) + ".tar.gz linux490@192.168.0.11:/home")
+        latestStable = max(versions)
+
+        packageName = selection + str(latestStable) + ".tar.gz"
+
+        os.system("scp " + "linux490@" + deploymentIP + ":/packages/" + packageName + " linux490@" + installIP + ":/packages")
+        os.system("ssh linux490@" + installIP + " 'tar xf /packages/" + packageName + " -C /packages/unzipped/'")
+        os.system("ssh linux490@" + installIP + " cp -a /packages/unzipped/" + selection.lower() + "/. /var/www/html")
+
+def build():
+    print("You can make a package for:")
+    for sections in Config.sections():
+        print(sections)
+
+    selection = input("\n Enter a package name to build: ")
+    directory = input("Enter your working directory e.g /home/directory/ (NOTE: USE TRAILING SLASH: \n")
+    isStable = input("is stable?(y/n): ")
+
+    if isStable == "y":
+        stable = 1
+    else:
+        stable = 0
+
+    mycursor = cnx.cursor()
+
+    mycursor.execute("SELECT version FROM versions WHERE type='" + selection + "'")
+
+    latest = mycursor.fetchall()
+
+    versions=[]
+    for version in latest:
+        versions.append(version[0])
+        print(versions)
+
+    prevVersion = max(versions)
+
+    newVersion = prevVersion + 1
+
+    sql = "INSERT INTO versions (filename, type, version, stable) VALUES (%s, %s, %s, %s)"
+    val = (selection + str(newVersion) + ".tar.gz", selection, newVersion, stable)
+    mycursor.execute(sql, val)
+    cnx.commit()
+
+    packageName = selection + str(newVersion) + ".tar.gz"
+
+    tar = tarfile.open(packageName, "w:gz")
+    for files in ConfigSectionMap(selection):
+        tar.add(directory + files)
+    tar.close()
+
+    os.system("scp " + selection + str(newVersion) + ".tar.gz linux490@" + deploymentIP + ":/packages")
+
+deploymentIP = input("Enter the IP of the deployment server (default is 192.168.0.11): ")
+if deploymentIP == "":
+    deploymentIP = "192.68.0.11"
+
+cnx = mysql.connector.connect(user='test', password='4321Password.',
+                              host=deploymentIP,
+                              database='deployment')
+
+buildInstall = input("Enter 1 for build and 2 for install?: ")
+if buildInstall == "1":
+    build()
+if buildInstall == "2":
+    install()
